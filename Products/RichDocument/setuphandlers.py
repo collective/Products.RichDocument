3068cbf73790c34cb3b40c87e0fb7c0d485c4178
benniboy <bs@kleinundpartner.at>
2014-08-21 10:25:31 +0200

    fixed circular dependencies

--- Products/RichDocument/setuphandlers.py
+++ Products/RichDocument/setuphandlers.py
@@ -6,13 +6,9 @@ from Products.SimpleAttachment.setuphandlers import registerAttachmentsFormContr
 def setupRichDocument(context):
     if context.readDataFile('richdocument_various.txt') is None:
         return
-    
+
     portal = context.getSite()
-    
-    qi = getToolByName(portal, 'portal_quickinstaller')
-    if not (qi.isProductInstalled('Products.SimpleAttachment') or qi.isProductInstalled('SimpleAttachment') or qi.isProductInstalled('Attachment support')):
-        qi.installProduct('Products.SimpleAttachment')
-    
+
     # Set up form controller actions for the widgets to work
     registerAttachmentsFormControllerActions(portal, contentType = 'RichDocument', template = 'atct_edit')
     registerImagesFormControllerActions(portal, contentType = 'RichDocument', template = 'atct_edit')
@@ -20,16 +16,16 @@ def setupRichDocument(context):
     # Register form controller actions for LinguaPlone translate_item
     registerAttachmentsFormControllerActions(portal, contentType = 'RichDocument', template = 'translate_item')
     registerImagesFormControllerActions(portal, contentType = 'RichDocument', template = 'translate_item')
-    
+
     # Make RichDocumnt objects linkable in kupu
     kupuTool = getToolByName(portal, 'kupu_library_tool', None)
     tinyTool = getToolByName(portal, 'portal_tinymce', None)
-    
+
     if kupuTool:
         linkable = list(kupuTool.getPortalTypesForResourceType('linkable'))
         if 'RichDocument' not in linkable:
             linkable.append('RichDocument')
-    
+
         # kupu_library_tool has an idiotic interface, basically written purely to
         # work with its configuration page. :-(
         kupuTool.updateResourceTypes(({'resource_type' : 'linkable',
